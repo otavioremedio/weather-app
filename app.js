c20de58efcfee88b5f992750105a75e6ffbf3744
otavioremedio <otavioremedio@gmail.com>
2017-07-21 20:50:33 -0300

    aula 7 modulo 4

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const request = require('request');
+const yargs = require('yargs');
+const argv = yargs
+  .options({
+    a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address= ${encodedAddress}` ,
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  //3 parametro é a quantidade de espaços
+  //console.log(JSON.stringify(response, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
 });
