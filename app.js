26294abd6102f732dc835fc0f079a3a32a8fa3f4
Otavio Remedio <otavioremedio@gmail.com>
2017-07-25 09:15:11 -0300

    25/07/2017

--- app.js
+++ app.js
@@ -21,3 +21,22 @@ const argv = yargs
       console.log(JSON.stringify(results, undefined, 2));
     }
   });
+
+//teza@storj99.top
+//
+
+//https://api.darksky.net/forecast/47f4f37bbe36fc88df571344ff772f4c/39.9396284,-75.18663959999999
+
+
+const request = require('request');
+
+request({
+    url: 'https://api.darksky.net/forecast/47f4f37bbe36fc88df571344ff772f4c/39.9396284,-75.18663959999999',
+    json: true
+  }, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      console.log(body.currently.temperature);
+    } else {
+      console.log('Unable to fetch weather.' )
+    }
+});
